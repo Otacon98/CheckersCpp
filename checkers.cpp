eeba247389e71ea3afaf5ded30b6ed3bf66a1368
Hendry M. Flores <hend_manel001@hotmail.com>
2017-12-22 10:10:41 -0400

    Se agrego Menu con dpad, adicion de funciones getch() y getch_(), menu en una funcon, funcion de imprimir fichas, y funcion de seleccion de fichas (mejorar), todo con las flechitas... (Validaciones hechas).
    La funcion getch(), usada para seleccionar la ficha, esta habilita, pero parece que ha una lireria nativa de linux donde esta
    incluida, la funcion de menu solo lo imprimie una vez, y llama a lafuncion de selector, la cual imprime un cursor, y segun su posicion devuelve un valor entre l uno y el tope dela funcion. Al igual que con el menu,
    el tablero se imprime una sola vez, la seleccion de la ficha y otras acciones sobre escriben al tablero,crear una
    estructura que tubiese las posiciones x,y, para siplificar las funciones, se simplifico la funcion de imprimir tablero, y se la hizo mas pequeña, el espacio a la derecha debe ser par imprimir las acciones que se realizan en el juego,
    validar que al presionar la tecla ESC se salga un menu de salir y guardarpartida (archivos) o si desea continuar.
    
    Se cambiaron los caracteres de las fichas, para hacer mas comoda la impresion en pantalla. La logica del juevgo debe
    desarrollarse en la matriz, la funcin de impresion esta validada para imprimir la matriz, sea cual sea (debe reimpimirse
    el tablero solo despues e efectuar un movimiento). la selccion del movimiento se hara con la misma funcion de seleccion.
    falta mejorar mucho el estilo, el comido no tiene buena estructura.

--- checkers.cpp
+++ checkers.cpp
@@ -8,35 +8,27 @@
 
 using namespace std;
 
-int main() {
-
-	setlocale(LC_ALL, "es_ES");
-	resizeTerminal();
-	short input = 0;
-	
-	while(input != 6){
+int menu (){
+		short input;
+		resizeTerminal();
 		clearScreen();
 		gotoxy(33,6);
 		cout << "\033[1;20mB I E N V E N I D O\033[0m\n";
-		gotoxy(30,7);
+		gotoxy(33,7);
 		cout << "\033[1;20mA  D A M A S  C H I N A S\033[0m\n";
-		gotoxy(30,10);
+		gotoxy(OTAB,10);
 		cout << "\033[1;38m[1].\033[0m Humano contra Humano\n";
-		gotoxy(30,11);
+		gotoxy(OTAB,11);
 		cout << "\033[1;38m[2].\033[0m Humano contra Bot\n";
-		gotoxy(30,12);
+		gotoxy(OTAB,12);
 		cout << "\033[1;38m[3].\033[0m Bot contra Bot\n";
-		gotoxy(30,13);
+		gotoxy(OTAB,13);
 		cout << "\033[1;38m[4].\033[0m Instrucciones\n";
-		gotoxy(30,14);
+		gotoxy(OTAB,14);
 		cout << "\033[1;38m[5].\033[0m Información\n";
-		gotoxy(30,15);
+		gotoxy(OTAB,15);
 		cout << "\033[1;38m[6].\033[0m Salir\n";
-		gotoxy(30,17);
-		cout << "\033[1;4m¿Qué desea hacer?:\033[0m\n";
-		gotoxy(51,17);
-		cin >> input;
-		
+		input=menuXY(10,6);
 		switch(input){
 			case 1:
 				humanoVShumano();
@@ -56,12 +48,15 @@ int main() {
 			case 6:
 				salir();
 				break;
-			default:
-				// Aqui se debe validar el buffer del input o alguna magia negra para que no explote
-				// cuando le tiras en el menu algo tipo "123456734980"
-				break;
-		}
+
 	}
+	return (1);
+}
+
+int main() {
+
+	setlocale(LC_ALL, "es_ES");
 	
+	menu();
 	return 0;
 }
--- gameLogic.hpp
+++ gameLogic.hpp
@@ -3,13 +3,28 @@
 #include <time.h>
 #include <wchar.h>
 #include <unistd.h>
-using namespace std;
+#include <termios.h>
 
-string blanco="░";
-string negro="█";
-string piezaBlanca = "O";
-string piezaNegra = "X";
-string vacio = " ";
+using namespace std;
+static struct termios old, new_;
+
+//"Aqui llego HendryXXXXXXXXX1 a tirar mierda en el codigo":
+#define KEY_UP 65
+#define KEY_DO 66
+#define KEY_DE 67
+#define KEY_IZ 68
+#define ENTER 10
+#define ESC 27
+#define CTAB 25//Posicion en el eje X del cursor
+#define OTAB 29//Posicion en el eje X de las opciones del menu
+#define cursor "\033[1;38m</>"
+
+string piezaBlanca="\E[41m░";
+string piezaNegra="\E[42m░";
+string blanco="\E[47m░";
+string negro="█";//
+string vacio=" ";
+string selector="\E[40m░";
 
 void resizeTerminal(){
 	cout << "\e[8;27;85t";
@@ -21,7 +36,80 @@ void clearScreen(){
 	cout << "\033[2J";
 }
 
-// Clase tablero
+//getch(); y getche();//Necesario para usar el "dpad"
+void initTermios(int echo) 
+{
+  tcgetattr(0, &old); /* grab old terminal i/o settings */
+  new_ = old; /* make new settings same as old settings */
+  new_.c_lflag &= ~ICANON; /* disable buffered i/o */
+  new_.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
+  tcsetattr(0, TCSANOW, &new_); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void) 
+{
+  tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo) 
+{
+  char ch;
+  initTermios(echo);
+  ch = getchar();
+  resetTermios();
+  return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void) 
+{
+  return getch_(0);
+}
+
+
+//###############################################################
+//########################     MENU     #########################
+//###############################################################
+
+
+int menuXY(int a, int A){
+	int KEY;
+	int y=a;
+	A=A+a;
+	do{
+		gotoxy(CTAB,y);
+		cout<<cursor;
+		KEY=getch();
+		switch (KEY)
+		{	
+			case KEY_UP:
+				gotoxy(CTAB,y);cout<<"   ";
+				y--;
+				if (y==(a-1))
+					y=(A-1);
+			break;
+			case KEY_DO:
+				gotoxy(CTAB,y);cout<<"   ";
+				y++;
+				if (y==A)
+					y=a;
+			break;
+			case ENTER:
+				return (y-a+1);
+			break;
+		}
+	}while (KEY!=ENTER);
+	return (y-a+1);
+}
+
+
+//###############################################################
+//####################     Clase tablero     ####################
+//###############################################################
+
+
 class Tablero{
 	
 	private:
@@ -33,351 +121,170 @@ class Tablero{
 			for(short i=0;i<8;i++){
 				for(short j=0;j<8;j++){
 					if( i<= 2 && ((i+j)%2 !=0) )
-						tablero[i][j] = piezaBlanca; 
-					else if (i>=5&&((i+j)%2!=0))
 						tablero[i][j] = piezaNegra; 
+					else if (i>=5&&((i+j)%2!=0))
+						tablero[i][j] = piezaBlanca; 
 					else 
 						tablero[i][j] = vacio;
 				}
 			}
 		}; 	
-		void imprimirCasillas();
 		void imprimirTablero();
-			
-			
+		void imprimirPiezas();
+		void imprimirCursor(int, int, string);
+		void seleccionarPieza(string);
+		void moverPieza();
+		int imprimirCasilla(int,int,int,int);
 };
-void Tablero::imprimirCasillas(){
-	// Esto es infernal de leer, analicen uno y modulenlo, como explique en la funcion "imprimirTablero"
-	
-	//Fila 1
-	for(int i = 0; i<3; i++ ){
-		gotoxy(0,i+1);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(6,i+1);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(11,i+1);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(16,i+1);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(21,i+1);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(26,i+1);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(31,i+1);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(36,i+1);
-		cout << negro << negro << negro << negro << negro;
-	}
-	
-	// Fila 2
-	for(int i = 0; i<3; i++ ){
-		gotoxy(0,i+4);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(6,i+4);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(11,i+4);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(16,i+4);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(21,i+4);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(26,i+4);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(31,i+4);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(36,i+4);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-
-	// Fila 3
-	for(int i = 0; i<3; i++ ){
-		gotoxy(0,i+7);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(6,i+7);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(11,i+7);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(16,i+7);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(21,i+7);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(26,i+7);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(31,i+7);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(36,i+7);
-		cout << negro << negro << negro << negro << negro;
-	}
-	
-	// Fila 4
-	for(int i = 0; i<3; i++ ){
-		gotoxy(0,i+10);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(6,i+10);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(11,i+10);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(16,i+10);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(21,i+10);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(26,i+10);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(31,i+10);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(36,i+10);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-
-	// Fila 5
-	for(int i = 0; i<3; i++ ){
-		gotoxy(0,i+13);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(6,i+13);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(11,i+13);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(16,i+13);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(21,i+13);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(26,i+13);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(31,i+13);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(36,i+13);
-		cout << negro << negro << negro << negro << negro;
-	}
-	
-	// Fila 6
-	for(int i = 0; i<3; i++ ){
-		gotoxy(0,i+16);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(6,i+16);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(11,i+16);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(16,i+16);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(21,i+16);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(26,i+16);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(31,i+16);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(36,i+16);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-
-	// Fila 7
-	for(int i = 0; i<3; i++ ){
-		gotoxy(0,i+19);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(6,i+19);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(11,i+19);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(16,i+19);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(21,i+19);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(26,i+19);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(31,i+19);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(36,i+19);
-		cout << negro << negro << negro << negro << negro;
-	}
-	
-	// Fila 8 - mamalo hendry	
-	for(int i = 0; i<3; i++ ){
-		gotoxy(0,i+22);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(6,i+22);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(11,i+22);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(16,i+22);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(21,i+22);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(26,i+22);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(31,i+22);
-		cout << negro << negro << negro << negro << negro;
-	}
-	for(int i = 0; i<3; i++ ){
-		gotoxy(36,i+22);
-		cout << blanco << blanco << blanco << blanco << blanco;
-	}
-	
-	
-	short i = 3;
+void Tablero::imprimirTablero(){
+	clearScreen();
+	int i,j,x=0,y=1;
+	for (i=1;i<=8;i++){
+		for (j=1,x=0;j<=8||x<36;j++){
+			x=imprimirCasilla(i,j,x,y)+7;
+		}
+		y=y+4;
+		x=0;
+	}
+	i = 4;
 	// Imprimiendo coordenadas horizontales
 	for(int a = 65; a<73; a++){ // A == 65 - ASCII code
-		gotoxy(i,26);
+		gotoxy(i,34);
 		cout << (char)a; 
-		i = i+5;
+		i = i+7;
 	}
-	
 	short aux = 2;
 	// Imprimiendo coordenadas verticales 
-	for(int i = 0;i<8;i++){
-		gotoxy(43,aux);
+	for(int i=8;i>=1;i--){
+		gotoxy(58,aux);
 		cout << i;
-		aux = aux +3;
+		aux = aux +4;
 	}
-
 }
-void Tablero::imprimirTablero(){
-	// Angel - 10/12/17 - 6:50pm
-	// Imprimiendo casillas -- Alguien que module esto para que sea mas amigable (con fors. yo lo estaba haciendo pero hay que 
-	// sentarse un rato para calcular las coordenadas del gotoxy para que se imprima bien
-	// mi idea es tener la funcion "imprimirTablero" que llame a "imprimirCasilla" y luego "imprimirPieza"
-	// quedando algo bastante agradable tipo:
-	//
-	//		imprimirCasillas();
-	//		imprimirPiezas(tablero);
-	//
-	//	Y quedaría una abstracción bastante chevere de lo que es la matriz que se muestra con respecto al tablero real donde estarán las piezas
-	//
-	
-	imprimirCasillas();
-	//imprimirPiezas(tablero);
-};	
+int Tablero::imprimirCasilla(int i,int j,int x, int y){
+	if ((i+j)%2==0){
+		for(int z=0;z<4;z++){
+			gotoxy(x,z+y);
+			cout<<blanco<<blanco<<blanco<<blanco<<blanco<<blanco<<blanco;
+		}
+	}
+	else{
+		for(int z=0;z<4;z++){
+			gotoxy(x,z+y);
+			cout <<negro<<negro<<negro<<negro<<negro<<negro<<negro;
+		}
+	}
+	if (x==0)
+		return(1);
+	else
+		return(x);
+}
+
+void Tablero::imprimirPiezas(){
+	int y=2, x=2;
+	string aux;
+	for(int i=0;i<=8;i++){
+		for (int j=0;j<8;j++){
+			if(this->tablero[i][j]==piezaBlanca){
+				gotoxy(x,y);
+				cout<<piezaBlanca<<piezaBlanca<<piezaBlanca<<piezaBlanca<<piezaBlanca;
+				gotoxy(x,y+1);
+				cout<<piezaBlanca<<piezaBlanca<<piezaBlanca<<piezaBlanca<<piezaBlanca;
+			}else if(this->tablero[i][j]==piezaNegra){
+				gotoxy(x,y);
+				cout<<piezaNegra<<piezaNegra<<piezaNegra<<piezaNegra<<piezaNegra;	
+				gotoxy(x,y+1);
+				cout<<piezaNegra<<piezaNegra<<piezaNegra<<piezaNegra<<piezaNegra;
+			}
+			x=x+7;
+		}
+		x=2;
+		y=y+4;
+	}
+}
+void Tablero::seleccionarPieza(string pieza){
+	int KEY,x=1, y=29, i=0, j=7;
+	bool cent=true;
+	do{
+		imprimirCursor(x,y,selector);
+		KEY=getch();
+		fflush(stdin);
+		if ((i+j)%2!=0)
+			imprimirCursor(x,y,negro);
+		if((i+j)%2==0)
+			imprimirCursor(x,y,blanco);
+		fflush(stdin);
+		switch (KEY){
+			case KEY_DO:
+				y=y+4;
+				j=j+1;
+				if (j>7){
+					y=1;
+					j=0;
+				}			
+			break;
+			case KEY_UP:
+				y=y-4;
+				j=j-1;
+				if (j<0){
+					y=29;
+					j=7;
+				}
+			break;
+			case KEY_IZ:
+				x=x-7;
+				i=i-1;
+				if(i<0){
+					x=50;
+					i=7;
+				}
+			break;
+			case KEY_DE:
+				x=x+7;
+				i=i+1;
+				if(i>7){
+					x=1;
+					i=0;
+				}
+			break;
+			case ENTER:
+				if (tablero[j][i]==pieza)
+					cent=false;
+			break;
+		}
+	}while(cent);
+	/*Llamado de la funcion de movimiento...
+	 * Busca los movimientos disponibles, si no existen sebe retornar a esta...
+	 * todo se esta enredando.. jeje
+	 */
+}
+
+void Tablero::imprimirCursor(int X, int Y,string casilla){
+	gotoxy(X,Y);
+	cout<<casilla<<casilla<<casilla<<casilla<<casilla<<casilla<<casilla;
+	gotoxy(X,Y+1);cout<<casilla;gotoxy(X+6,Y+1);cout<<casilla;		
+	gotoxy(X,Y+2);cout<<casilla;gotoxy(X+6,Y+2);cout<<casilla;
+	gotoxy(X,Y+3);
+	cout<<casilla<<casilla<<casilla<<casilla<<casilla<<casilla<<casilla;
+}
+
+
+//###############################################################
+//###################     Otras Funciones     ###################
+//###############################################################
+
 
 void humanoVShumano(){
 	
-	char coordH;
-	int turno = 1;
-	Tablero tablero;
+	//char coordH;
+	//int turno = 1;
+	Tablero tablero=Tablero();
 	clearScreen();
 	tablero.imprimirTablero();
+	tablero.imprimirPiezas();
+	tablero.seleccionarPieza(piezaBlanca);
 	
-	gotoxy(55, 5);
-	cout << "Turno del jugador " << turno;
 	
-	gotoxy(55, 6);
-	cout << "Sus fichas son: O";
-	
-	gotoxy(55,7);
-	cout << "Esperando jugada";
-	
-	gotoxy(47,10);
-	cout << "Introduzca primera coordenada (A-H)";
-	
-	gotoxy(65,11);
-	cin >> coordH;
 }
 
 void instrucciones(){
